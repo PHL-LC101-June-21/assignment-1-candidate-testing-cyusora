0cec70389c58ebe60af47759e4a655201bd58e8b
Courtney <85703772+cyusora@users.noreply.github.com>
2021-06-22 02:13:30 +0000

    initialized questions and correctAnswers array and added/edited code to add for loop that returns questions one by one, stores answers, and returns incorrect/correct

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,15 +2,16 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+
 // TODO 1.1a: Define candidateName // 
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?', '(5 + 3)/2 * 10 = ?', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 'What is the minimum crew size for the ISS?'];   
+let correctAnswers = ['Sally Ride', ' true', ' 40', ' Trajectory', ' 3'];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -20,10 +21,18 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-const input = require('readline-sync');
-candidateAnswer = input.question(question);
 
+for (let i = 0; i < questions.length; i++) {
+  let userInput = input.question(questions[i]);
+  candidateAnswers.push(String(userInput));
+  if (candidateAnswers[i] !== correctAnswers[i]) {
+  console.log(`Sorry. ${candidateAnswers[i]} is incorrect. The correct answer is ${correctAnswers[i]}`);
+} else {
+  console.log('Correct!');
 }
+} 
+}
+
 
 function gradeQuiz(candidateAnswers) {
 
@@ -43,7 +52,7 @@ if (candidateAnswer === correctAnswer) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log('Hello, ' + candidateName + '!');
+  console.log('Hello, ' + candidateName + '! Let us take this quiz!');
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
